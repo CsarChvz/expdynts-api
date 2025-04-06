5bc674072233d8de9e878450be4be60a9bfed613
CsarChvz <csarchvz@192.168.1.73>
2025-04-05 20:09:07 -0600

    Added drizzle

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,19 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 
 import fastifyCaching from '@fastify/caching';
 import compression from '@fastify/compress';
+import fastifyEtag from '@fastify/etag';
+import fastifyCors from '@fastify/cors';
+
+const logger = {
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      translateTime: 'HH:MM:ss Z',
+      ignore: 'pid,hostname',
+      colorize: true,
+    },
+  },
+};
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -11,18 +24,24 @@ async function bootstrap() {
     new FastifyAdapter({ logger: false }), // Habilita logs de Fastify
   );
 
-  await app.register(compression, { 
-    encodings: ['gzip', 'br'], 
-    threshold: 1024, 
+  await app.register(fastifyCors);
+  await app.register(compression, {
+    encodings: ['gzip', 'br'],
+    threshold: 5120,
+  });
+
+  await app.register(fastifyEtag, {
+    weak: true,
   });
 
   await app.register(fastifyCaching, {
-    privacy: 'public', // Cache público (ej: para CDNs)
-    expiresIn: 3600, // TTL de 1 hora
+    privacy: 'private',
+    expiresIn: 3600,
   });
 
-
   await app.listen(3000, '0.0.0.0');
-  console.log(`Servidor corriendo en http://localhost:3000 con Bun y Fastify 🚀`);
+  console.log(
+    `Servidor corriendo en http://localhost:3000 con Bun y Fastify 🚀`,
+  );
 }
 bootstrap();
