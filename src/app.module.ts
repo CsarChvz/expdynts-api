978a5df9904ababb7d572ead6f10714496520d3b
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2025-04-16 17:27:29 -0600

    a

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { PostsModule } from "./posts/posts.module";
 import { CommonModule } from "./common/common.module";
+import { QueueModule } from './module/queue/queue.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PostsModule,
     CommonModule,
+    QueueModule,
   ],
 })
 export class AppModule {}
