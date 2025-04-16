70e3b3ae220cb0804b966b8698c8c2e798759ea1
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2025-04-16 16:27:07 -0600

    definiticion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PostsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PostsModule, CommonModule],
 })
 export class AppModule {}
