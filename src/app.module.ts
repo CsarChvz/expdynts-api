5241df051646b2e25b5576cbacfdfe61c8a3741e
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2025-04-16 09:19:34 -0600

    deleted things

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
