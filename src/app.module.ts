ba4c2cbe0024ec70dfb84ff158b17f3e9f7dae4d
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2025-04-16 12:44:49 -0600

    deleted health

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
-import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PostsModule, HealthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PostsModule],
 })
 export class AppModule {}
